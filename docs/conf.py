edd427dd5c82219015210553df46a7d123b52673
Deepak Cherian <deepak@cherian.net>
2025-07-08 22:54:19 -0700

    silence warning

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,7 @@ intersphinx_mapping = {
     "shapely": ("https://shapely.readthedocs.io/en/latest/", None),
     "xvec": ("https://xvec.readthedocs.io/en/stable/", None),
     "xdggs": ("https://xdggs.readthedocs.io/en/latest/", None),
-    "geopandas": ("https://geopandas.readthedocs.io/en/stable/", None),
+    "geopandas": ("https://geopandas.org/en/stable/", None),
     "pint-xarray": ("https://pint-xarray.readthedocs.io/en/latest/", None),
     "pint": ("https://pint.readthedocs.io/en/stable/", None),
 }
